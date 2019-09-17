4dbe79c7b677daaefa603d034c9c845f5a2faa24
jwenger <j@jwenger-z390-aorus-pro.mvl6.uni-tuebingen.de>
2019-09-17 16:28:52 +0200

    added automatic installation of dependencies

--- setup.py
+++ setup.py
@@ -16,8 +16,21 @@ setup(
     description='Non-parametric Calibration for Classification',
     long_description=readme,
     author='Jonathan Wenger',
-    author_email='j.wenger@tum.de',
+    author_email='jonathan.wenger@uni-tuebingen.de',
     url='https://github.com/JonathanWenger/pycalib',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs', 'data', 'figures'))
+    packages=find_packages(exclude=('tests', 'docs', 'data', 'figures')),
+    install_requires=[
+        'betacal>=0.2.7',
+        'gpflow>=1.3.0',
+        'matplotlib>=3.1.0',
+        'numpy>=1.15.4',
+        'pandas>=0.23.4',
+        'scikit-learn>=0.20.1',
+        'scipy>=1.3.0',
+        'tensorflow>=1.12.2',
+        'torch>=0.4.1',
+        'pretrainedmodels>=0.7.4'
+    ],
+    dependency_links=['https://github.com/scikit-garden/scikit-garden.git#egg=skgarden']
 )
