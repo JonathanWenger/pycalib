ec3e0d57541116513293140191ab53dd86d279b0
jwenger <j@jwenger-z390-aorus-pro.mvl6.uni-tuebingen.de>
2019-09-18 13:56:21 +0200

    .

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
 
 with open('README.md') as f:
@@ -20,7 +16,8 @@ setup(
     url='https://github.com/JonathanWenger/pycalib',
     license=license,
     packages=find_packages(exclude=('tests', 'docs', 'data', 'figures')),
-    install_requires=[
+    install_requires=
+    [
         'betacal>=0.2.7',
         'gpflow>=1.3.0',
         'matplotlib>=3.1.0',
@@ -31,8 +28,9 @@ setup(
         'tensorflow>=1.12.2',
         'torch>=0.4.1',
         'pretrainedmodels>=0.7.4',
-        'skgarden @ git+https://github.com/scikit-garden/scikit-garden.git#egg=skgarden'
+        'scikit-garden @ git+ssh://git@github.com/scikit-garden/scikit-garden@master#egg=scikit-garden'
     ]
-    #,
- #   dependency_links=['https://github.com/scikit-garden/scikit-garden.git#egg=skgarden']
 )
+
+# Installation dependency from github:
+# https://stackoverflow.com/questions/32688688/how-to-write-setup-py-to-include-a-git-repo-as-a-dependency
