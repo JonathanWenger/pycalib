7ed4c48e784a4be2d4b06e39923640a83139cf51
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2019-09-20 18:22:59 +0200

    updated experiments to inlude linear gpcalib

--- benchmark/ImageNet.py
+++ benchmark/ImageNet.py
@@ -73,6 +73,9 @@ if __name__ == "__main__":
     cal_methods = {
         "Uncal": calm.NoCalibration(),
         "GPcalib": calm.GPCalibration(n_classes=1000, maxiter=1000, n_inducing_points=10, random_state=random_state),
+        "GPcalib_lin": calm.GPCalibration(n_classes=1000, maxiter=1000, n_inducing_points=10,
+                                          mean_function=meanfunc, logits=False, verbose=False,
+                                          random_state=random_state),
         # "Temp": calm.TemperatureScaling(),
         "Isotonic": calm.IsotonicRegression(),
         "Beta": calm.BetaCalibration(),
